9d138378474169ac6b66e8a17e58e1cdba4f1f8e
xuemin-yu <graceyxm0215@gmail.com>
2023-05-25 17:51:08 -0300

    Adding the CLS prediction

--- layer_code.sh
+++ layer_code.sh
@@ -2,7 +2,8 @@
 
 scriptDir=scripts
 inputPath=data # path to a sentence file
-input=input_100.in #name of the sentence file
+dataset=sst2
+
 # model name or path to a finetuned model
 model="glue-cased-models"
 
@@ -16,22 +17,20 @@ mkdir ${outputDir}
 
 working_file=$input.tok.sent_len #do not change this
 
+#5. Extract layer-wise activations
 source activate neurox_pip
-
-# 3. Extract layer-wise activations
 python -m neurox.data.extraction.transformers_extractor --decompose_layers --filter_layers ${layer} --output_type json ${model} ${working_file} ${outputDir}/${working_file}.activations.json --include_special_tokens
 
-#4. Create a dataset file with word and sentence indexes
+#6. Create a dataset file with word and sentence indexes
 python ${scriptDir}/create_data_single_layer.py --text-file ${working_file}.modified --activation-file ${outputDir}/${working_file}.activations-layer${layer}.json --output-prefix ${outputDir}/${working_file}-layer${layer}
 
-#6. Filter number of tokens to fit in the memory for clustering. Input file will be from step 4. minfreq sets the minimum frequency. If a word type appears is coming less than minfreq, it will be dropped. if a word comes
+#7. Filter number of tokens to fit in the memory for clustering. Input file will be from step 4. minfreq sets the minimum frequency. If a word type appears is coming less than minfreq, it will be dropped. if a word comes
 minfreq=5
 maxfreq=20
 delfreq=20
 python ${scriptDir}/frequency_filter_data.py --input-file ${outputDir}/${working_file}-layer${layer}-dataset.json --frequency-file ${working_file}.words_freq --sentence-file ${outputDir}/${working_file}-layer${layer}-sentences.json --minimum-frequency $minfreq --maximum-frequency $maxfreq --delete-frequency ${delfreq} --output-file ${outputDir}/${working_file}-layer${layer}
 
-#7. Run clustering
-
+#8. Run clustering
 conda activate clustering
 
 mkdir ${outputDir}/results
@@ -49,3 +48,8 @@ python -u ${scriptDir}/extract_data.py --input-file $DATASETPATH --output-path $
 echo "Creating Clusters!"
 python -u ${scriptDir}/get_agglomerative_clusters.py --vocab-file $VOCABFILE --point-file $POINTFILE --output-path $RESULTPATH  --cluster $CLUSTERS --range 1
 echo "DONE!"
+
+#9. Extract the prediction for CLS token
+mkdir ${outputDir}/CLS_tokens
+source activate explanation
+python ${scriptDir}/extract_cls_prediction.py --dataset-name-or-path ${inputPath}/$dataset --model-name ${model} --tokenizer-name ${model} --save-dir ${outputDir}/CLS_tokens/ --layer ${layer}
